5baa56d841a859538b116135f69df171d6ded8c9
Jose Ozuna <joseozuna48@gmail.com>
2023-09-18 14:11:53 -0700

    Modify how operations are made

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
-let displayText = "0";
 let num1 = "";
 let num2 = "";
 let operator = "";
+let result = "";
 let clearDisplay = false;
 
 let display = document.querySelector(".display");
@@ -48,18 +48,17 @@ function operate(operator, num1, num2) {
 
 function numberListener(){
 
-    if(display.textContent == 0 || clearDisplay ){
-        displayText = "";
+    if(display.textContent == "0" || clearDisplay ){
+        display.textContent = 0;
         clearDisplay = false;
     }
 
-    displayText += this.dataset.value;
-    display.textContent = displayText;
-
     if(operator){
-        num2= +displayText;
+        num2= num2 +""+ this.dataset.value;
+        display.textContent = num2;
     }else{
-        num1= +displayText;
+        num1=  num1 + "" + this.dataset.value;
+        display.textContent = num1;
     }
 }
 
@@ -67,7 +66,7 @@ function resetCalculator(){
     num1="";
     num2="";
     operator="";
-    displayText = "0";
+    result = "";
 
     display.textContent="0";
 }
@@ -83,19 +82,21 @@ function deleteLast(){
 }
 
 function operationListener(){
-    
-    if(!operator){
+
+    if(!operator && num1){
         operator = this.dataset.value;
         clearDisplay = true;
     }
 }
 
 function equals(){
-    result = operate(operator,num1,num2);
-
-    resetCalculator();
-    num1=result;
-    display.textContent=num1;
+    if(operator && num1 && num2){
+        let operation = operate(operator,+num1,+num2);
+        resetCalculator();
+        result = operation;
+        display.textContent=result;
+    }
+    
 }
 
 
